b12619d2964f65b76953058735e8c638dd385b03
Shashank Phatkure <shashankphatkure@gmail.com>
2024-11-08 04:14:25 +0530

    updated

--- components/providers/SupabaseProvider.js
+++ components/providers/SupabaseProvider.js
@@ -1,6 +1,33 @@
 "use client";
-import { SessionProvider } from "next-auth/react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { SessionProvider, useSession } from "next-auth/react";
+
+const SupabaseContext = createContext();
+
+function SupabaseProviderInternal({ children }) {
+  const supabase = createClientComponentClient();
+  const { data: session } = useSession();
+
+  return (
+    <SupabaseContext.Provider value={{ supabase, session }}>
+      {children}
+    </SupabaseContext.Provider>
+  );
+}
 
 export default function SupabaseProvider({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <SupabaseProviderInternal>{children}</SupabaseProviderInternal>
+    </SessionProvider>
+  );
+}
+
+export function useSupabase() {
+  const context = useContext(SupabaseContext);
+  if (!context) {
+    throw new Error("useSupabase must be used within a SupabaseProvider");
+  }
+  return context;
 }
